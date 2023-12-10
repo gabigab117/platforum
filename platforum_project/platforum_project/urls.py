a5bb05306a15f60b3c20a5960ccf2ffee5d34f94
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-10 21:39:26 +0100

    add a rout to landing app

--- platforum_project/platforum_project/urls.py
+++ platforum_project/platforum_project/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
 
 from platforum_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('landing.urls')),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
