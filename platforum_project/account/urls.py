5e38103ba63b8d7e88c08347c40a25d82927ee87
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-12 22:54:26 +0100

    add login and logout view

--- platforum_project/account/urls.py
+++ platforum_project/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import signup
+from .views import signup, LoginUser, logout_view
 
 
 app_name = "account"
 urlpatterns = [
     path('signup/', signup, name="signup"),
+    path('login/', LoginUser.as_view(), name="login"),
+    path('logout/', logout_view, name="logout"),
 ]
--- platforum_project/account/views/user.py
+++ platforum_project/account/views/user.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 
 from account.forms import SignUpForm
 
 
 def signup(request):
-    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("landing:index")
+        # Penser à revenir pour ajouter un message de confirmation
+    else:
+        form = SignUpForm()
     return render(request, "account/signup.html", context={"form": form})
+
+
+class LoginUser(LoginView):
+    template_name = "account/login.html"
+    next_page = reverse_lazy("landing:index")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("landing:index")
