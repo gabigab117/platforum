8bfeb742a65673e04f6faf87ef5972b9b3cd9aea
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-13 21:48:16 +0100

    forum master with PROTECT

--- platforum_project/forum/models/forum.py
+++ platforum_project/forum/models/forum.py
@@ -1,10 +1,9 @@
 from django.db import models
 from platforum_project.settings import AUTH_USER_MODEL
-from forum.func.default_value import DEFAULT_ADMIN
 
 
 class Forum(models.Model):
-    forum_master = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.SET_DEFAULT, default=DEFAULT_ADMIN,
+    forum_master = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.PROTECT,
                                      verbose_name="Administrateur")
     name = models.CharField(max_length=100, verbose_name="Nom du Forum")
     description = models.CharField(max_length=1000, verbose_name="Description")
