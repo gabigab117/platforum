5af1560349ecbc513348fa72f5da6b21c050f978
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-13 21:22:01 +0100

    add personal messagin

--- platforum_project/forum/models/content.py
+++ platforum_project/forum/models/content.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class Topic(models.Model):
     title = models.CharField(max_length=100, verbose_name="Titre")
-    category = models.ForeignKey(to=Category, on_delete=models.SET_DEFAULT)
+    category = models.ForeignKey(to=Category, on_delete=models.SET_NULL, null=True)
     # Si le modérateur souhaite clôturer le sujet sans le supprimer
     closed = models.BooleanField(default=False, verbose_name="Clôturé")
     user = models.ForeignKey(to=AUTH_USER_MODEL, verbose_name="Auteur", on_delete=models.SET_NULL, null=True)
@@ -23,8 +23,21 @@ class Topic(models.Model):
 class Message(models.Model):
     message = models.CharField(max_length=10000, verbose_name="Message")
     user = models.ForeignKey(to=AUTH_USER_MODEL, verbose_name="Auteur", on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(to=Topic, verbose_name="Sujet", on_delete=models.CASCADE)
+    topic = models.ForeignKey(to=Topic, verbose_name="Sujet", on_delete=models.CASCADE, null=True)
+    personal_messaging = models.ForeignKey(to="PersonalMessaging", on_delete=models.CASCADE,
+                                           verbose_name="Messagerie Personnel", null=True)
+    personal = models.BooleanField(verbose_name="Personnel", default=False)
 
     @property
     def author(self):
         return self.user.username if self.user else "Utilisateur banni"
+
+
+class PersonalMessaging(models.Model):
+    user_one = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Utilisateur 1",
+                                 related_name="user_one_personal_messaging")
+    user_two = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Utilisateur 2",
+                                 related_name="user_two_personal_messaging")
+
+    def __str__(self):
+        return f"Discussion entre {self.user_one} et {self.user_two}"
