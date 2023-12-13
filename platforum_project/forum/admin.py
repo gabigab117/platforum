b3de703213cd611ed7f7c5b3b05e4600beb08e80
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-13 21:47:41 +0100

    add in admin

--- platforum_project/forum/admin.py
+++ platforum_project/forum/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Forum, ForumAccount, Category, Topic, Message, PersonalMessaging
 
-# Register your models here.
+
+admin.site.register(Forum)
+admin.site.register(ForumAccount)
+admin.site.register(Category)
+admin.site.register(Topic)
+admin.site.register(Message)
+admin.site.register(PersonalMessaging)
