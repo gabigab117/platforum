d1baaf1dd96c9ccff0bbf88635b6a07a8825a275
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-15 00:13:37 +0100

    ad index in urls

--- platforum_project/forum/urls.py
+++ platforum_project/forum/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import create_forum
+from .views import create_forum, index
 
 app_name = "forum"
 urlpatterns = [
     path('create-forum/', create_forum, name="create-forum"),
-
+    path('index/', index, name="index"),
 ]
