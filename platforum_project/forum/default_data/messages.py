1187e3c0b74f4fc8eb81f66b9d4a9c81158a64d7
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-14 01:04:43 +0100

    default message

---
+++ platforum_project/forum/default_data/messages.py
@@ -0,0 +1,7 @@
+def welcome_message(user):
+    return (f"Bienvenu(e) {user.username} !\n"
+            f"Ton forum est crée. Tu n'as plus qu'à créer des catégories.\n"
+            f"N'hésites pas à aller voir sur le forum officiel de PlatForum pour t'inspirer (lien ici).\n"
+            f"Il est important de respecter (et faire respecter) les règles pour que tout le monde puisse communiquer en toute harmonie.\n"
+            f"Pour bien utiliser le constructeur de forum n'hésites pas à aller voir la documentation (lien)\n"
+            f"Tu peux évidemment supprimer cette catégorie test dès à présent pour créer le forum à ton image et à l'image du thème choisi.")
