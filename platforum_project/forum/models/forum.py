45e7141afb1b9eed76e77dd49af0664468577e8c
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-15 00:14:09 +0100

    add a save method in forum for slug

--- platforum_project/forum/models/forum.py
+++ platforum_project/forum/models/forum.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+
 from platforum_project.settings import AUTH_USER_MODEL
 
 
@@ -6,12 +8,18 @@ class Forum(models.Model):
     forum_master = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.PROTECT,
                                      verbose_name="Administrateur")
     name = models.CharField(max_length=100, verbose_name="Nom du Forum")
+    slug = models.SlugField(blank=True)
     theme = models.ForeignKey(to="Theme", verbose_name="Thème", on_delete=models.PROTECT)
     description = models.CharField(max_length=1000, verbose_name="Description")
     creation = models.DateField(auto_now_add=True, verbose_name="Date de création")
 
     def __str__(self):
         return f"{self.name} - {self.forum_master.username}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 
 class Theme(models.Model):
