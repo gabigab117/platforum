57d2142b4724a2dee2da0a15231117e5d897e715
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-14 23:53:01 +0100

    add views to create forum

--- platforum_project/forum/views.py
+++ platforum_project/forum/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .forms import CreateForumForm
+from .default_data.default_forum_data import create_forum_with_data
 
 
 @login_required
 def create_forum(request):
-    # Créer en cascade en une fois : Forum, AccountForum de l'admin, Categorie Test, Sujet Test
+    user = request.user
+    if request.method == "POST":
+        form = CreateForumForm(request.POST)
+        if form.is_valid():
+            create_forum_with_data(form, user)
+
     form = CreateForumForm()
     return render(request, "forum/create-forum.html", context={"form": form})
