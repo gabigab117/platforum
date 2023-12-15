3321a6804409ba4d6b7af5824e0d795af0ecfd9a
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-15 01:16:00 +0100

    add context to view forum

--- platforum_project/forum/views/forum.py
+++ platforum_project/forum/views/forum.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+
+from forum.models import Forum, Category, ForumAccount, SubCategory, Topic, Message
 
 
+@login_required
 def index(request, slug):
-    return render(request, "forum/index.html")
+    # Afficher les catégories et sous catégories
+    user = request.user
+    forum = get_object_or_404(Forum, slug=slug)
+    categories = Category.objects.filter(forum=forum)
+    return render(request, "forum/index.html", context={"forum": forum, "categories": categories})
