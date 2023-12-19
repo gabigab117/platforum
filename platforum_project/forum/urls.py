b5b9b681f70ec688e0f9342eb6cd95c15469e320
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-20 00:52:42 +0100

    ad url to tpic view

--- platforum_project/forum/urls.py
+++ platforum_project/forum/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import create_forum, index, sub_category_view, add_topic
+from .views import create_forum, index, sub_category_view, add_topic, topic_view
 
 app_name = "forum"
 urlpatterns = [
     path('create-forum/', create_forum, name="create-forum"),
     path('<str:slug>/index/', index, name="index"),
     path('<str:slug_forum>/sub-category/<int:pk>/<str:slug_sub_category>', sub_category_view, name="sub-category"),
-    path('<str:slug_forum>/<int:pk>/<str:slug_sub_category>/add/', add_topic, name="add-topic")
+    path('<str:slug_forum>/<int:pk>/<str:slug_sub_category>/add/', add_topic, name="add-topic"),
+    path('<str:slug_forum>/<int:pk>/<str:slug_sub_category>/<int:pk_topic>/<str:slug_topic>', topic_view, name="topic"),
 ]
