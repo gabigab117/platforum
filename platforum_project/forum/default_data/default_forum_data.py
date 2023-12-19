932b6417f66cb193ab608c8d515eb7226614c02a
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-20 00:14:29 +0100

    use commit False

--- platforum_project/forum/default_data/default_forum_data.py
+++ platforum_project/forum/default_data/default_forum_data.py
@@ -2,8 +2,9 @@ from forum.models import ForumAccount, Category, SubCategory, Topic, Message
 
 
 def create_forum_with_data(form, user):
-    form.instance.forum_master = user
-    forum = form.save()
+    forum = form.save(commit=False)
+    forum.forum_master = user
+    forum.save()
     ForumAccount.objects.create(forum=forum, user=user)
     category = Category.create_test_category(forum)
     sub_category = SubCategory.create_test_subcategory(category)
