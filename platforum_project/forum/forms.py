f4286b674d329b30b8a628ff88522f59c497f4ad
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-20 00:14:38 +0100

    add form to add topic

--- platforum_project/forum/forms.py
+++ platforum_project/forum/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 
-from forum.models import Forum
+from forum.models import Forum, Topic
 
 
 class CreateForumForm(forms.ModelForm):
     class Meta:
         model = Forum
         fields = ["name", "theme", "description"]
+
+
+class CreateTopic(forms.ModelForm):
+    message = forms.CharField(widget=forms.Textarea, label="Message", max_length=1000)
+
+    class Meta:
+        model = Topic
+        fields = ["title", "message"]
