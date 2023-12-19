f8365b1b086b82fdd309143ea0b5c99d917c2799
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-20 00:52:31 +0100

    topic view ok

--- platforum_project/forum/views/forum.py
+++ platforum_project/forum/views/forum.py
@@ -46,3 +46,17 @@ def add_topic(request, slug_forum, pk, slug_sub_category):
         "sub_category": sub_category,
         "form": form
     })
+
+
+def topic_view(request, slug_forum, pk, slug_sub_category, pk_topic, slug_topic):
+    user = request.user
+    forum = get_object_or_404(Forum, slug=slug_forum)
+    sub_category = get_object_or_404(SubCategory, pk=pk)
+    topic = get_object_or_404(Topic, pk=pk_topic)
+    messages = Message.objects.filter(topic=topic)
+    return render(request, "forum/topic.html", context={
+        "forum": forum,
+        "sub_category": sub_category,
+        "topic": topic,
+        "messages": messages
+    })
