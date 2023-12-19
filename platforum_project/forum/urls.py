e610258a8d3638ea06a4b901a2293489dc536de8
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-20 00:15:08 +0100

    add new topic view

--- platforum_project/forum/urls.py
+++ platforum_project/forum/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_forum, index, sub_category_view
+from .views import create_forum, index, sub_category_view, add_topic
 
 app_name = "forum"
 urlpatterns = [
     path('create-forum/', create_forum, name="create-forum"),
-    path('index/<str:slug>', index, name="index"),
+    path('<str:slug>/index/', index, name="index"),
     path('<str:slug_forum>/sub-category/<int:pk>/<str:slug_sub_category>', sub_category_view, name="sub-category"),
+    path('<str:slug_forum>/<int:pk>/<str:slug_sub_category>/add/', add_topic, name="add-topic")
 ]
--- platforum_project/forum/views/forum.py
+++ platforum_project/forum/views/forum.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from forum.models import Forum, Category, ForumAccount, SubCategory, Topic, Message
+from forum.forms import CreateTopic
 
 
-@login_required
 def index(request, slug):
     # Afficher les catégories et sous catégories
     user = request.user
@@ -14,7 +14,35 @@ def index(request, slug):
 
 
 def sub_category_view(request, pk, slug_forum, slug_sub_category):
+    # Afficher la liste des sujets (pagination)
+    # Bouton nouveau sujet
     forum = get_object_or_404(Forum, slug=slug_forum)
     sub_category = get_object_or_404(SubCategory, pk=pk)
+    topics = Topic.objects.filter(sub_category=sub_category)
     return render(request, "forum/sub-category.html", context={"sub_category": sub_category,
-                                                               "forum": forum})
+                                                               "forum": forum,
+                                                               "topics": topics})
+
+
+def add_topic(request, slug_forum, pk, slug_sub_category):
+    user = request.user
+    forum = get_object_or_404(Forum, slug=slug_forum)
+    sub_category = get_object_or_404(SubCategory, pk=pk)
+
+    if request.method == "POST":
+        form = CreateTopic(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.user = user
+            topic.sub_category = sub_category
+            topic.save()
+            Message.objects.create(message=form.cleaned_data["message"], user=user, topic=topic)
+            return redirect("landing:index")
+
+    else:
+        form = CreateTopic()
+    return render(request, "forum/new-topic.html", context={
+        "forum": forum,
+        "sub_category": sub_category,
+        "form": form
+    })
