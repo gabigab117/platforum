70e8314736313674b85fed56c274a3e9fa4853cd
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-26 19:23:02 +0100

    add blank to accountforum model for thumbnail and update thumbnail clean

--- platforum_project/forum/models/account.py
+++ platforum_project/forum/models/account.py
@@ -9,7 +9,7 @@ from platforum_project.settings import AUTH_USER_MODEL
 class ForumAccount(models.Model):
     forum = models.ForeignKey(to=Forum, on_delete=models.CASCADE, verbose_name="Forum")
     user = models.ForeignKey(to=AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Utilisateur")
-    thumbnail = models.ImageField(upload_to="avatars", verbose_name="Vignette", null=True)
+    thumbnail = models.ImageField(upload_to="avatars", verbose_name="Vignette", null=True, blank=True)
     # En cas de modération le modérateur peut désactiver le compte temporairement
     active = models.BooleanField(verbose_name="Actif", default=True)
     joined = models.DateField(verbose_name="Rejoins le", auto_now_add=True)
@@ -22,7 +22,7 @@ class ForumAccount(models.Model):
         return self.thumbnail.url if self.thumbnail else static("default/default_thumbnail.png")
 
     def clean(self):
-        if self.thumbnail.size > 5 * 1024 * 1024:
+        if self.thumbnail and self.thumbnail.size > 5 * 1024 * 1024:
             raise ValidationError("La taille du fichier ne doit pas dépasser 5MO.")
 
     class Meta:
