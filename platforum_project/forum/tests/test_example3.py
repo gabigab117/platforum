e844c005769876584da2f6d2208b6dfe4dc21839
gabigab117 <gabrieltrouve5@gmail.com>
2023-12-31 11:17:28 +0100

    start learning pytest to replace unittest

---
+++ platforum_project/forum/tests/test_example3.py
@@ -0,0 +1,9 @@
+# import pytest
+#
+# from account.models import CustomUser
+#
+#
+# @pytest.mark.django_db
+# def test_user_create():
+#     user = CustomUser.objects.create_user("gabouche", "g@g.com", "tr", "gab", "12345678")
+#     assert user.username == "gabouche"
