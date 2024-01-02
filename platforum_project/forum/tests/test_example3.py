8d4b9649490ec773192e9cff01d91282d42e75cf
gabigab117 <gabrieltrouve5@gmail.com>
2024-01-03 00:02:39 +0100

    i'm testing... pytest !

--- platforum_project/forum/tests/test_example3.py
+++
@@ -1,9 +0,0 @@
-# import pytest
-#
-# from account.models import CustomUser
-#
-#
-# @pytest.mark.django_db
-# def test_user_create():
-#     user = CustomUser.objects.create_user("gabouche", "g@g.com", "tr", "gab", "12345678")
-#     assert user.username == "gabouche"
