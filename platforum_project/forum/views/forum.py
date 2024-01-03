e907470b8c58dbfb1d363f410bb09716bcbd4cbd
gabigab117 <gabrieltrouve5@gmail.com>
2024-01-04 00:01:52 +0100

    add a security with forum account

--- platforum_project/forum/views/forum.py
+++ platforum_project/forum/views/forum.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_POST
 from django.forms import model_to_dict
 from django.shortcuts import render, get_object_or_404, redirect
 
-from platforum_project.func.security import user_permission
+from platforum_project.func.security import user_permission, user_has_active_forum_account
 from forum.models import Forum, Category, ForumAccount, SubCategory, Topic, Message
 from forum.forms import CreateTopic, PostMessage
 
@@ -28,9 +28,11 @@ def sub_category_view(request, pk, slug_forum, slug_sub_category):
                                                                "topics": topics, "pin_topics": pin_topics})
 
 
+@login_required
 def add_topic(request, slug_forum, pk, slug_sub_category):
     user = request.user
     forum = get_object_or_404(Forum, slug=slug_forum)
+    account = user_has_active_forum_account(user, forum)
     sub_category = get_object_or_404(SubCategory, pk=pk)
 
     if request.method == "POST":
@@ -80,12 +82,13 @@ def topic_view(request, slug_forum, pk, slug_sub_category, pk_topic, slug_topic)
     })
 
 
+@login_required
 def update_message(request, slug_forum, pk, slug_sub_category, pk_topic, slug_topic, pk_message):
     user = request.user
     forum = get_object_or_404(Forum, slug=slug_forum)
     sub_category = get_object_or_404(SubCategory, pk=pk)
     topic = get_object_or_404(Topic, pk=pk_topic)
-    account = ForumAccount.objects.get(forum=forum, user=user)
+    account = user_has_active_forum_account(user, forum)
     message = get_object_or_404(Message, pk=pk_message)
     user_permission(message, user)
 
@@ -106,6 +109,7 @@ def update_message(request, slug_forum, pk, slug_sub_category, pk_topic, slug_to
     })
 
 
+@login_required
 @require_POST
 def delete_message(request, pk_topic, pk_message):
     message = get_object_or_404(Message, pk=pk_message)
--- platforum_project/platforum_project/func/security.py
+++ platforum_project/platforum_project/func/security.py
@@ -1,6 +1,15 @@
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
+from forum.models import ForumAccount
 
 
 def user_permission(forum_element, user):
     if forum_element.user != user:
+        raise PermissionDenied()
+
+
+def user_has_active_forum_account(user, forum):
+    try:
+        account = ForumAccount.objects.get(user=user, forum=forum, active=True)
+        return account
+    except ObjectDoesNotExist:
         raise PermissionDenied()
