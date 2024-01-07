d4e6fd73afb9018a4515f7f0d8dd0cac2967965d
gabigab117 <gabrieltrouve5@gmail.com>
2024-01-07 23:49:18 +0100

    add forum list voiew with search engine

--- platforum_project/landing/urls.py
+++ platforum_project/landing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from landing.views import index
+from landing.views import index, forums_list_view
 
 app_name = "landing"
 urlpatterns = [
     path('', index, name="index"),
+    path('forums-list/', forums_list_view, name="forums-list")
 ]
--- platforum_project/landing/views.py
+++ platforum_project/landing/views.py
@@ -1,5 +1,19 @@
+from django.db.models import Q
 from django.shortcuts import render
+
+from forum.models import Forum
 
 
 def index(request):
     return render(request, "landing/index.html")
+
+
+def forums_list_view(request):
+    forums = Forum.objects.all()
+
+    search = request.GET.get("rechercher-forum")
+    if search:
+        forums = Forum.objects.filter(
+            Q(name__icontains=search) | Q(theme__name__icontains=search) | Q(description__icontains=search)
+        )
+    return render(request, "landing/forums-list.html", context={"forums": forums})
