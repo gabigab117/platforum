93e09541732aae71640535e61ca36f7ead07d4da
gabigab117 <gabrieltrouve5@gmail.com>
2024-01-09 09:59:56 +0100

    finish profile view

--- platforum_project/account/views/user.py
+++ platforum_project/account/views/user.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
 from account.forms import SignUpForm
+from forum.models import ForumAccount, Message, Topic
 
 
 def signup(request):
@@ -23,11 +25,17 @@ class LoginUser(LoginView):
     next_page = reverse_lazy("landing:index")
 
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect("landing:index")
 
 
+@login_required
 def profile_view(request):
     user = request.user
-    return render(request, "account/profile.html", context={})
+    forum_accounts = ForumAccount.objects.filter(user=user).count()
+    messages = Message.objects.filter(account__user=user).count()
+    topics = Topic.objects.filter(account__user=user).count()
+    return render(request, "account/profile.html", context={"forum_accounts": forum_accounts,
+                                                            "messages": messages, "topics": topics})
