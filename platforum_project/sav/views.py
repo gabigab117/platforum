c7584945e43e863f094d4853b9fcd9a38dd1e019
gabigab117 <gabrieltrouve5@gmail.com>
2024-01-10 00:11:54 +0100

    create sav app

---
+++ platforum_project/sav/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def contact_view(request):
+    user = request.user
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            subject = form.cleaned_data["subject"]
+            text = form.cleaned_data["text"]
+            send_mail(subject=subject, message=f"De la part de {email} - {text}",
+                      recipient_list=["gabrieltrouve5@yahoo.com"], from_email=None)
+            send_mail(subject="Email bien envoyé", message="J'ai bien reçu votre email, je réponds rapidement :).",
+                      from_email=None, recipient_list=[email])
+            messages.add_message(request, messages.INFO,
+                                 "Le message a été envoyé, si vous ne recevez pas d'email "
+                                 "de confirmation veuillez vérifier vos spams ou renvoyer votre "
+                                 "message en vérifiant bien l'email renseigné svp.")
+            return redirect("sav:contact")
+    else:
+        form = ContactForm(initial={"email": user.email}) if user.is_authenticated else ContactForm()
+
+    return render(request, "sav/contact.html", context={"form": form})
