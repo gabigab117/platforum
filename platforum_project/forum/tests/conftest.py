1964efd2fa5dd29c885cad8983de3a607c0f0b52
gabigab117 <gabrieltrouve5@gmail.com>
2024-01-13 01:52:37 +0100

    add fixtures to create a complete starter forum and add signup test

--- platforum_project/forum/tests/conftest.py
+++ platforum_project/forum/tests/conftest.py
@@ -1,8 +1,38 @@
 import pytest
 
-from forum.models import Theme
+from forum.models import Theme, Forum, ForumAccount, Category, SubCategory, Topic, Message
 
 
 @pytest.fixture
 def theme_1(db):
     return Theme.objects.create(name="dev")
+
+
+@pytest.fixture
+def forum_1(db, user_1, theme_1):
+    return Forum.objects.create(forum_master=user_1, name="Metal", theme=theme_1, description="Le forum du metal")
+
+
+@pytest.fixture
+def forum_master_account_1(db, forum_1, user_1):
+    return ForumAccount.objects.create(forum=forum_1, user=user_1, forum_master=True)
+
+
+@pytest.fixture
+def category_1(db, forum_1):
+    return Category.objects.create(name="Catégorie1", forum=forum_1)
+
+
+@pytest.fixture
+def sub_category_1(db, category_1):
+    return SubCategory.objects.create(name="SousCatégorie1", category=category_1)
+
+
+@pytest.fixture
+def topic_1(db, sub_category_1, forum_master_account_1):
+    return Topic.objects.create(title="Titre1", sub_category=sub_category_1, account=forum_master_account_1)
+
+
+@pytest.fixture
+def message_1(db, forum_master_account_1, topic_1):
+    return Message.objects.create(message="message1", account=forum_master_account_1, topic=topic_1)
--- platforum_project/forum/tests/test_views.py
+++ platforum_project/forum/tests/test_views.py
@@ -9,23 +9,39 @@ from forum.models import Forum, Theme, ForumAccount, Category, SubCategory, Topi
 
 @pytest.mark.django_db
 def test_create_forum_view_post(client: Client, theme_1, user_1):
+    # Given an user wants to create a forum
     client.login(username=user_1.username, password="12345678")
+    # When user validates the form
     response = client.post(reverse("forum:create-forum"), data={"name": "devforum",
                                                                 "theme": theme_1.id,
                                                                 "description": "Forum des devs",
                                                                 "thumbnail": ""})
-
+    # Then a Forum is created with default data
     forum = Forum.objects.get(name="devforum")
-    account = ForumAccount.objects.get(forum=forum)
+    account: ForumAccount = ForumAccount.objects.get(forum=forum)
     category = Category.objects.get(name="Catégorie Test", forum=forum)
     sub_category = SubCategory.objects.get(name="Sous catégorie Test", category=category)
     topic = Topic.objects.get(title="Bienvenu(e)", sub_category=sub_category, account=account)
     message = Message.objects.get(account=account, topic=topic, message=welcome_message(account))
     assert forum.name == "devforum"
     assert account.user == user_1
+    assert account.forum_master
     assert category.forum == forum
     assert sub_category.category == category
     assert topic.account == account
     assert "Ton forum est crée" in message.message
     assert response.status_code == 302
     assertRedirects(response, reverse("forum:index", args=["devforum", 1]))
+
+
+@pytest.mark.django_db
+def test_signup_post(client: Client, forum_1, user_2):
+    client.login(username=user_2.username, password="12345678")
+    data = {"thumbnail": ""}
+    response = client.post(reverse("forum:signup", args=["metal", 1]), data=data)
+    assertRedirects(response, reverse("forum:profile", args=["metal", 1]))
+    account: ForumAccount = ForumAccount.objects.get(forum=forum_1, user=user_2)
+    assert account.user.username == "pat"
+    assert not account.forum_master
+    assert account.active
+    assert account.forum.name == "Metal"
