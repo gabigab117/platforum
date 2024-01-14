98da75eb9cb3f55de4581cf1c3f9279a9086750f
gabigab117 <gabrieltrouve5@gmail.com>
2024-01-14 18:17:31 +0100

    add email address in my settings

--- platforum_project/platforum_project/settings.py
+++ platforum_project/platforum_project/settings.py
@@ -188,7 +188,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_ID')
+EMAIL_HOST_USER = 'gabrieltrouve5@gmail.com'
 EMAIL_HOST_PASSWORD = env('EMAIL_PW')
 DEFAULT_FROM_EMAIL = 'PlatForum<gabrieltrouve5@gmail.com>'
 
