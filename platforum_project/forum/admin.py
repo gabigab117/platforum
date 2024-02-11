e5f32786d6ea1eb580bd2f2e1d5919fbde60d7ac
gabigab117 <gabrieltrouve5@gmail.com>
2024-02-11 13:37:46 +0100

    add Theme

--- platforum_project/forum/admin.py
+++ platforum_project/forum/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Forum, ForumAccount, Category, Topic, Message, Conversation, SubCategory, Notification, Badge, Like
-
+from .models import Forum, ForumAccount, Category, Topic, Message, Conversation, SubCategory, Notification, Badge, Like, \
+    Theme
 
 admin.site.register(Forum)
 admin.site.register(ForumAccount)
@@ -12,3 +12,4 @@ admin.site.register(SubCategory)
 admin.site.register(Notification)
 admin.site.register(Badge)
 admin.site.register(Like)
+admin.site.register(Theme)
